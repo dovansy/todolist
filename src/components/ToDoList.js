f8eef1afe565d3857226199df3e2d933e8cc7686
dovansy <sydv@omt.com.vn>
2022-03-19 11:54:37 +0700

    add animation

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -4,6 +4,11 @@ import AddTodo from "./AddTodo";
 
 function ToDoList() {
   const [showDetail, setShowDetail] = useState(false);
+  const [listCheck, setListCheck] = useState([]);
+  const checkedTodo = (item) => {
+    setListCheck([...listCheck, item]);
+    console.log(listCheck);
+  };
   return (
     <div className="todo-list">
       <div>
@@ -15,7 +20,7 @@ function ToDoList() {
             <div className="to-do-item">
               <div className="to-do-item-header">
                 <div className="item-side-left">
-                  <input type="checkbox" className="form-check-input mr-2" />
+                  <input type="checkbox" className="form-check-input mr-2" id="1" onChange={(e) => checkedTodo(e.target.id)} />
                   <span className="form-check-label">Do homework</span>
                 </div>
                 <div className="item-side-right">
@@ -33,15 +38,19 @@ function ToDoList() {
             )}
           </div>
         </div>
-        <div className="bulk-action px-2">
-          <div className="item-side-left">
-            <span className="form-check-label">Bulk action:</span>
+        {listCheck.length !== 0 && (
+          <div className="bulk-action px-2">
+            <div className="item-side-left">
+              <span className="form-check-label">Bulk action:</span>
+            </div>
+            <div className="item-side-right">
+              <button className="btn btn-primary btn-action mx-2" onClick={() => console.log(listCheck)}>
+                Done
+              </button>
+              <button className="btn btn-danger btn-action">Remove</button>
+            </div>
           </div>
-          <div className="item-side-right">
-            <button className="btn btn-primary btn-action mx-2">Done</button>
-            <button className="btn btn-danger btn-action">Remove</button>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
